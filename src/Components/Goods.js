1f96dadcde4cf656459e12c85bb04ac23769e6d5
IluhaByrm81 <iluhaburm@yandex.ru>
2021-09-20 16:26:06 +0300

    pagination done bye on crutches

--- src/Components/Goods.js
+++ src/Components/Goods.js
@@ -4,19 +4,44 @@ import { getGoods } from "./api";
 
 function Goods() {
   const [goods, setGoods] = useState([]);
+  const [page, setPage] = useState(0);
+  const [limit] = useState(2);
+  const [validNextPage, setValidNextPage] = useState(true);
+  const [validPrevPage, setValidPrevPage] = useState(false);
 
   useEffect(() => {
-    getGoods()
+    getGoods(page, limit)
       .then((goods) => {
         setGoods(goods);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [page, limit]);
+
+  const nextPage = () => {
+    if (page !== limit) {
+      setPage(page + 1);
+      setValidPrevPage(true);
+      setValidNextPage(true);
+    } else {
+      setValidNextPage(false);
+    }
+  };
+
+  const prevPage = () => {
+    if (page && limit) {
+      setPage(page - 1);
+      setValidNextPage(false);
+      setValidNextPage(true);
+    } else {
+      setValidPrevPage(false);
+    }
+  };
 
   return (
     <div className="wraper">
+      {page}
       {goods.map((elem) => {
         return (
           <div key={elem.id}>
@@ -26,6 +51,12 @@ function Goods() {
           </div>
         );
       })}
+      <button onClick={prevPage} disabled={!validPrevPage}>
+        Назад
+      </button>
+      <button onClick={nextPage} disabled={!validNextPage}>
+        Вперед
+      </button>
     </div>
   );
 }
